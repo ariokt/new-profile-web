7fdbbce028442c33641cb0c1f18f59068a2cd620
web.designer <web.designer@loanmarket.co.id>
2023-06-27 23:52:56 +0700

    update navbar

--- src/sections/navbar/index.js
+++ src/sections/navbar/index.js
@@ -26,7 +26,7 @@ const Navbar = () => {
 
     return (
         <nav className={"navbar flex items-center justify-between px-4 md:px-24 lg:px-36 py-4 fixed w-full h-fit " + (scrolled ? "active" : "inactive") }>
-            <a className="navbar__logo navbar__link p-2" href="./">Ario Waskita</a>
+            <Link className="navbar__logo navbar__link p-2" href="./">Ario Waskita</Link>
             <div className="flex">
                 <a className="navbar__link p-2" href="https://drive.google.com/file/d/1ZSpDDLkirP9VY-Cwsid_KY-7Wg5GY-kq/view?usp=share_link" target='_blank'>Resume</a>
                 <Link className="navbar__link p-2" href="./gallery">Gallery</Link>
