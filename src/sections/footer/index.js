88d59a23c0c22d969a02b865d812df81d6ab89ef
ariokt <ariowta@gmail.com>
2024-01-06 14:21:24 +0700

    responsive year footer

--- src/sections/footer/index.js
+++ src/sections/footer/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+'use client'
+import React, { useEffect, useState } from "react";
 import "./index.css";
 import WhatsappMe from "@/components/whatsappme";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -8,6 +9,13 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 const Footer = () => {
     const sosmed = [{icon: faLinkedin, link:"https://www.linkedin.com/in/ario-waskita-318b3a151/"}, {icon: faSquareFacebook, link:"https://www.facebook.com/ario.waskita.9/"},
                     {icon: faSquareInstagram, link:"https://www.instagram.com/ariowta/"}, {icon: faEnvelope, link:"mailto:ariowta@gmail.com"}];
+
+    const [year, setYear] = useState('');
+
+    useEffect(()=> {
+        setYear(new Date().getFullYear());
+    }, [])
+    
     return (
         <footer className="py-7 bg-black">
             <div className="px-6 md:px-8 flex flex-col items-center gap-4 md:gap-8">
@@ -19,7 +27,7 @@ const Footer = () => {
                     {sosmed.map((item, y) => <a href={item.link} target="_blank" key={y} className="text-white hover:text-fuchsia-500"><FontAwesomeIcon icon={item.icon} size="2xl"/></a>)}
                 </div>
                 <div className="footer-headline">
-                    <h4 align="center" className="mb-2">&copy; {new Date().getFullYear()} Ario Waskita</h4>
+                    <h4 align="center" className="mb-2">&copy; {year} Ario Waskita</h4>
                 </div>
             </div>
         </footer>
