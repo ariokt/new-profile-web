3d5df49ef189d436b3c404f0364b543b1cfb95a4
ariokt <ariowta@gmail.com>
2026-03-04 13:35:55 +0700

    feat: animation using GSAP

--- src/sections/projectsection/index.js
+++ src/sections/projectsection/index.js
@@ -1,23 +1,42 @@
-import React from "react";
+'use client'
+
+import React, { useRef } from "react";
 import Image from 'next/image';
-import ss1 from "../../assets/projects/ss1.webp";
-import ss2 from "../../assets/projects/ss2.webp";
-import ss3 from "../../assets/projects/ss3.webp";
-import ss4 from "../../assets/projects/ss4.webp";
 import data from "../../../dataProjects";
 import "./index.css";
 import Link from "next/link";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const ProjectSection = () => {
+    const sectionRef = useRef(null);
 
-    const images = [{img:ss1,link:"https://drive.google.com/drive/folders/1pWeceiXFuNPFyCP3uXtQvb1ym4UF-RgJ?usp=share_link"}, {img:ss2,link:"https://www.loanmarket.co.id/licence-opportunity-ads"}, {img:ss3,link:"https://drive.google.com/drive/folders/1sWKiwt5-Nrz-vvTCLUinp3d3g62-uM3G?usp=share_link"}, {img:ss4,link:"https://alamsutera.loanmarket.co.id/"}];
+    useGSAP(() => {
+        const tl = gsap.timeline({
+            scrollTrigger: {
+              trigger: sectionRef.current,
+              start: "top 80%",
+              toggleActions: "play none none none",
+            },
+          });
+
+          tl.from(".project-card", {
+            opacity: 0,
+            x: (i) => (i % 2 === 0 ? -200 : 200),
+            duration: 0.7,
+            ease: "power3.out",
+            stagger: 0.4,
+          });
+    }, {
+        scope: sectionRef
+    })
 
     return (
-        <section className="projects flex flex-col items-center bg-black h-fit pt-7 px-8">
+        <section ref={sectionRef} className="projects flex flex-col items-center bg-black h-fit pt-7 px-8">
             <h3 className="mx-auto w-fit text-xl sm:text-3xl text-indigo-300 mb-2">My Projects</h3>
             <h2 className="mx-auto w-fit text-white text-xl sm:text-3xl lg:text-5xl font-semibold text-center">A selection of project that improve me.</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-12 lg:w-10/12 my-8 md:my-12">
-                {data.map((item,y) => <Link href={`/projects/${item.id}`} key={item.id} className="transition-transform duration-500 project-img rounded-3xl overflow-hidden border-2 border-gray-400 hover:border-2 hover:border-white"><Image src={item.thumbnail} alt="Ario Waskita Portofolio" style={{maxHeight:"280px"}} /></Link>)}
+                {data.map((item,y) => <Link href={`/projects/${item.id}`} key={item.id} className="transition-transform duration-500 project-img rounded-3xl overflow-hidden border-2 border-gray-400 hover:border-2 hover:border-white"><div className="project-card"><Image src={item.thumbnail} alt="Ario Waskita Portofolio" style={{maxHeight:"280px"}} /></div></Link>)}
             </div>
             <div className="md:grid-cols-2 flex flex-col gap-3 mb-12">
                 <h3 className="text-white mx-auto w-fit text-xl sm:text-3xl text-center">Wanna see more project? <a href="https://github.com/ariokt" target="_blank" className="text-indigo-300">Visit my github</a></h3>
--- src/sections/quotesection/index.js
+++ src/sections/quotesection/index.js
@@ -1,26 +1,68 @@
-import React from "react";
+'use client';
+import React, { useRef } from "react";
 import "./index.css";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const QuoteSection = () => {
-    return (
-        <section className="quote-section flex flex-col items-center bg-black">
-            <div className="h-full bg-orange-300 opacity-70 mb-4">
-                <div className="line h-full"></div>
-            </div>
-            <div className="px-8 md:px-40 mb-8">
-                <div>
-                    <h3 align="center" className="text-white text-3xl sm:text-5xl lg:text-7xl xl:text-8xl leading-normal">Design is not just what it looks like and feels like.</h3>
-                </div>
-                <div>
-                    <h3 align="center" className="text-3xl sm:text-5xl lg:text-7xl xl:text-8xl second-part-quote mb-4">It is how it works.</h3>
-                    <p align="center" className="text-2xl sm:text-4xl text-white opacity-70">- Steve Jobs</p>
-                </div>
-            </div>
-            <div className="h-full opacity-70 justify-self-end bg-purple-600">
-                <div className="line h-full"></div>
-            </div>
-        </section>
-    )
-}
+  const sectionRef = useRef(null);
 
-export default QuoteSection
+  useGSAP(() => {
+    const tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: sectionRef.current,
+            start: "top 80%",
+            toggleActions: "play none none none",
+        }
+    });
+
+    tl.to('.curtain-left', {
+        width: '0%',
+        duration: 1,
+        ease: "power3.out"
+    })
+    tl.to('.curtain-right', {
+        width: '0%',
+        duration: 1,
+        ease: "power3.out"
+    }, "<")
+  })
+  return (
+    <section ref={sectionRef} className="quote-section flex flex-col items-center bg-black relative">
+      <div className="curtain-left absolute top-0 left-0 w-1/2 h-full bg-gradient-to-r from-purple-900 via-black to-black z-20 border-r border-purple-500 shadow-[0_0_40px_#a855f7]" />
+      <div className="curtain-right absolute top-0 right-0 w-1/2 h-full bg-gradient-to-l from-purple-900 via-black to-black z-20 border-l border-purple-500 shadow-[0_0_40px_#a855f7]" />
+      <div className="h-full bg-orange-300 opacity-70 mb-4">
+        <div className="line h-full"></div>
+      </div>
+      <div className="px-8 md:px-40 mb-8">
+        <div>
+          <h3
+            align="center"
+            className="text-white text-3xl sm:text-5xl lg:text-7xl xl:text-8xl leading-normal"
+          >
+            Design is not just what it looks like and feels like.
+          </h3>
+        </div>
+        <div>
+          <h3
+            align="center"
+            className="text-3xl sm:text-5xl lg:text-7xl xl:text-8xl second-part-quote mb-4"
+          >
+            It is how it works.
+          </h3>
+          <p
+            align="center"
+            className="text-2xl sm:text-4xl text-white opacity-70"
+          >
+            - Steve Jobs
+          </p>
+        </div>
+      </div>
+      <div className="h-full opacity-70 justify-self-end bg-purple-600">
+        <div className="line h-full"></div>
+      </div>
+    </section>
+  );
+};
+
+export default QuoteSection;
